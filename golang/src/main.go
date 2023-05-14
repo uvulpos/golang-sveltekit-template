42bebe990fb92489a88bb5545f1d346354bf7bab
Tim Riedl <mail@tim-riedl.de>
2023-05-15 00:42:44 +0200

    feat: implement goreleaser locally

--- golang/src/main.go
+++ golang/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
@@ -10,10 +11,19 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 )
 
+var appversion string = "dev"
+var appbuildcommit string = "_"
+var appbuilddate string
+
 //go:embed frontend/*
 var svelteFS embed.FS
 
 func main() {
+
+	fmt.Println("Version ", appversion)
+	fmt.Println("Build-Commit ", appbuildcommit)
+	fmt.Println("Build-Date ", appbuilddate)
+
 	app := fiber.New()
 
 	publicFS, err := fs.Sub(svelteFS, "frontend")
