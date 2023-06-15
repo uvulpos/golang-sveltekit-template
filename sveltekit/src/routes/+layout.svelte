59652e06f7154842609cf9c4c8a8e18662f01dd0
Tim Riedl <mail@tim-riedl.de>
2023-06-16 01:45:43 +0200

    improve stuff

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script>
+  export const prerender = false;
+  export const ssr = false;
+  export const trailingSlash = "always";
+
   import Footer from "../lib/components/layout/footer.svelte";
   import Header from "../lib/components/layout/header.svelte";
+
+  import { addMessages, getLocaleFromNavigator, init } from "svelte-i18n";
+
+  import en from "./../lib/i18n/en.json";
+  import de from "./../lib/i18n/de.json";
+
+  addMessages("en", en);
+  addMessages("de", de);
+
+  init({
+    fallbackLocale: "en",
+    initialLocale: getLocaleFromNavigator(),
+  });
 </script>
 
 <div class="root">
