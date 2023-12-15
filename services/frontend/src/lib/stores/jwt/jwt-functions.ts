c81313939f58ea13774a3d7bb5f87e9ece6ee44e
Tim Riedl <mail@tim-riedl.de>
2023-12-15 08:37:41 +0100

    feat: create login system error handling

--- services/frontend/src/lib/stores/jwt/jwt-functions.ts
+++ services/frontend/src/lib/stores/jwt/jwt-functions.ts
@@ -1,38 +1,46 @@
 import { jwtStore } from "./jwt";
 import { goto } from "$app/navigation";
 
-export async function loginUser(username: string, password: string) {
-    // request the backend
+export async function loginUser(username: string, password: string): Promise<boolean> {
+    try {
+        // request the backend
+        let request: Response
+        let statusCode: number
+        let responseText: string
+        request = await fetch("/api/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            headers: new Headers({ 'content-type': 'application/json' }),
+        })
+        const awaitedResponse = await request
+        statusCode = awaitedResponse.status
+        responseText = await awaitedResponse.text()
 
-    const request = await fetch("http://web.localhost/api/login", {
-        method: "POST",
-        body: JSON.stringify({
-            username: username,
-            password: password
-        }),
-        headers: new Headers({ 'content-type': 'application/json' }),
-    })
+        // handle errors
+        if (!request.ok || statusCode < 200 || statusCode > 299) {
+            // do error handling
+            console.log("error, statuscode:", statusCode);
+            console.log(responseText);
+            return false
+        }
 
-    // handle errors
-    const awaitedResponse = await request
-    const statusCode = awaitedResponse.status
-    const responseText = await awaitedResponse.text()
-    if (!request.ok || statusCode < 200 || statusCode > 299) {
-        // do error handling
-        console.log("error, statuscode:", statusCode);
-        console.log(responseText);
-        return
+        // set jwt
+        let now: Date = new Date()
+        let expires = now
+        expires.setFullYear(2024)
+        jwtStore.set({
+            jwtToken: responseText,
+            authExpires: expires,
+            authCreated: now,
+            authRefreshed: now
+        })
+        console.log("no error");
+        goto("/dashboard")
+        return true
+    } catch (error) {
+        return false
     }
-
-    // set jwt
-    let now: Date = new Date()
-    let expires = now
-    expires.setFullYear(2024)
-    jwtStore.set({
-        jwtToken: responseText,
-        authExpires: expires,
-        authCreated: now,
-        authRefreshed: now
-    })
-    goto("/dashboard")
 }
