f87935e835f13963f3f905113fc20ea4454cacaf
Tim Riedl <mail@tim-riedl.de>
2024-01-07 07:42:29 +0100

    feat: add user role information in settings (#126)

---
+++ services/frontend/src/routes/(dashboard)/admin-settings/user-management/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  // i18n
+  import { _ } from "svelte-i18n";
+</script>
+
+<svelte:head>
+  <title>{$_("page.titles.settings")}</title>
+</svelte:head>
+
+<div class="header">
+  <div class="content">
+    <p>{$_("page.admin-settings.user-management.user-management")}</p>
+  </div>
+</div>
+
+<style lang="sass">
+      @import "../../../../lib/variables/sass/main"
+  
+      .header
+          height: 5rem
+          width: 100%
+          background-color: $ui-background-secondary
+          position: relative
+          
+          .content
+              position: absolute
+              transform: translate(-50%,-50%)
+              top: 50%
+              left: 50%
+              p
+                  font-weight: bolder
+                  font-size: 2rem
+</style>
