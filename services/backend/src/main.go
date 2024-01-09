c49e8b713aa368eac9b4f8102a70b99d6c30801f
Tim Riedl <mail@tim-riedl.de>
2024-01-09 15:10:59 +0100

    Update main.go

--- services/backend/src/main.go
+++ services/backend/src/main.go
@@ -6,14 +6,5 @@ import (
 )
 
 func main() {
-
-	// mailer := sendmail.NewMailer("mail.tim-riedl.de", 465, "sysintern@tim-riedl.de", "sysintern@tim-riedl.de", "4bbT::VQSZvUjS:iZLDn:mxTVQXFX3WnTL,PLDVqJRz:DWTdgJV3CPDen.7;4R;DrtqNFh")
-
-	// mail := mailer.NewMail()
-	// mail.SetReceipiants([]string{"mail@tim-riedl.de"})
-	// mail.SetSubject("hallo")
-	// mail.SetHTMLMessage("hallo welt")
-	// mail.SendMail()
-
 	cmd.Execute()
 }
