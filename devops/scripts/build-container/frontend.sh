9c172a3b5a6a04799c402d0bf1944770b2987bff
Tim Riedl <mail@tim-riedl.de>
2024-02-18 01:03:51 +0100

    feat: improve developer + devops tooling (#161)

---
+++ devops/scripts/build-container/frontend.sh
@@ -0,0 +1,3 @@
+## execute from root of the project (/golang-sveltekit-binary)/
+
+docker build -t mysinglebinaryimage:latest -f ./devops/dockerfiles/Dockerfile.frontend .
---
+++ devops/scripts/utils/install-dependencies.sh
@@ -0,0 +1,2 @@
+(cd ./services/frontend ; npm i)
+(cd ./services/backend ; go mod download && go mod tidy)
