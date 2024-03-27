cc582e68368baf7cafd1d456cb193903343ca8ff
Tim Riedl <mail@tim-riedl.de>
2024-03-27 20:35:38 +0100

    rename folder

--- devops/deployments/server-infrastructure/civo-k3s/main.tf
+++ devops/deployments/server-infrastructure/civo-k3s/main.tf
@@ -1,6 +1,27 @@
 variable "civo_kubernetes_size" {}
 variable "civo_kubernetes_count" {}
 
+# # Create Object Storage Credetials for Bucket to store backups
+# data "civo_object_store_credential" "backup" {
+#   name = "application-backup-bucket-credentials"
+# }
+
+# # Create Object Storage for Bucket to store backups
+# resource "civo_object_store" "backup" {
+#   name          = "application-backup-bucket"
+#   max_size_gb   = 500
+#   access_key_id = civo_object_store_credential.backup.access_key_id
+# }
+
+# # Create a managaed Database
+# resource "civo_database" "custom_database" {
+#   name    = "custom_database"
+#   size    = element(data.civo_size.small.sizes, 0).name
+#   nodes   = 2
+#   engine  = element(data.civo_database_version.mysql.versions, 0).engine
+#   version = element(data.civo_database_version.mysql.versions, 0).version
+# }
+
 # Create a firewall
 resource "civo_firewall" "my-firewall" {
   name = "application-firewall"
