68005a7aec752bfce429ebd96130b7b309d3db49
Tim Riedl <mail@tim-riedl.de>
2024-04-16 18:36:36 +0200

    wip add terraform backend

--- devops/deployments/server-infrastructure/civo/packages.tf
+++ devops/deployments/server-infrastructure/civo/packages.tf
@@ -1,7 +1,23 @@
-variable "civo_token" {}
-variable "civo_region" {}
+variable "civo_token" { type = string }
+variable "civo_region" { type = string }
 
 terraform {
+  backend "s3" {
+    endpoints {
+      s3 = "https://objectstore.fra1.civo.com/"
+    }
+    bucket                      = ""
+    key                         = ""
+    region                      = "FRA1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    use_path_style              = true
+    # access_key                  = var.tfbackend_objectstorage_accesskey
+    # secret_key                  = var.tfbackend_objectstorage_secretkey
+  }
+
   required_providers {
     civo = {
       source  = "civo/civo"
