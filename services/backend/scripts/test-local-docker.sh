51fb40bf6220fd45c481f595c9d880ecf431e269
Tim Riedl <mail@tim-riedl.de>
2024-04-19 22:11:08 +0200

    fix: install swag

--- services/backend/scripts/test-local-docker.sh
+++ services/backend/scripts/test-local-docker.sh
@@ -1,5 +1,6 @@
 go mod download 
 go install golang.org/x/tools/cmd/goimports
+go install github.com/swaggo/swag/cmd/swag@latest
 go install gotest.tools/gotestsum@latest
 
 swag fmt -g src/web-app/app.go && swag init -g src/web-app/app.go -o swagger-docs
