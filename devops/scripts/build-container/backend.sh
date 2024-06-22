5d919ba8136eb60df9e1d5a75a24b36caa762474
Tim Riedl <mail@tim-riedl.de>
2024-06-23 01:17:48 +0200

    add fix for container image build process

--- devops/scripts/build-container/backend.sh
+++ devops/scripts/build-container/backend.sh
@@ -1,3 +1,3 @@
 ## execute from root of the project (/golang-sveltekit-binary)/
 
-docker build -t mysinglebinaryimage:latest -f ./devops/dockerfiles/Dockerfile.backend .
+docker build -t mybackendimage:latest -f ./devops/dockerfiles/Dockerfile.backend .
