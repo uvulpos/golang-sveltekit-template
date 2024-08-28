2b52ce44fc8affa291faa689c500cf6fc5c5de09
Tim Riedl <mail@tim-riedl.de>
2024-08-28 16:47:42 +0200

    .

--- services/frontend/src/routes/(authentication)/logout/+page.server.ts
+++ services/frontend/src/routes/(authentication)/logout/+page.server.ts
@@ -2,6 +2,5 @@ import { redirect } from "@sveltejs/kit";
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies }) {
-    // cookies.delete("jwt", { path: "/" });
     throw redirect(301, "/api/v1/oauth/logout");
 }
